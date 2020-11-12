aee123a274c78e20ba4e25f753d3f1871822240d
Juan Molina Jiron <jmolinaj@fiu.edu>
2020-11-12 11:34:10 -0500

    Added log in and log out functionality to application. Routes according to loggedin state to Home component or HomeGuest component. Used localstorage to cache information to the browsers storage to remember user information when signed in.

--- app/components/App.js
+++ app/components/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
 import Header from './Header'
@@ -6,14 +6,18 @@ import HomeGuest from './HomeGuest'
 import Footer from './Footer'
 import About from './About'
 import Terms from './Terms'
+import Home from './Home'
 
 function App() {
+	const [loggedIn, setLoggedIn] = useState(
+		Boolean(localStorage.getItem('complexappToken'))
+	)
 	return (
 		<BrowserRouter>
-			<Header />
+			<Header loggedIn={loggedIn} setLoggedIn={setLoggedIn} />
 			<Switch>
 				<Route path='/' exact>
-					<HomeGuest />
+					{loggedIn ? <Home /> : <HomeGuest />}
 				</Route>
 				<Route path='/about-us'>
 					<About />
--- app/components/HomeGuest.js
+++ app/components/HomeGuest.js
@@ -1,8 +1,27 @@
-import React, { useEffect } from 'react'
-import Container from './Container'
+import React, { useState } from 'react'
+import Axios from 'axios'
+import { useImmer } from 'use-immer'
 import Page from './Page'
 
 function HomeGuest() {
+	const [username, setUsername] = useState()
+	const [email, setEmail] = useState()
+	const [password, setPassword] = useState()
+
+	async function handleSubmit(e) {
+		e.preventDefault()
+		try {
+			await Axios.post('http://localhost:8080/register', {
+				username,
+				email,
+				password
+			})
+			console.log('User was successfully created.')
+		} catch (error) {
+			console.log('error', error)
+		}
+	}
+
 	return (
 		<Page title='Welcome' wide={true}>
 			<div className='row align-items-center'>
@@ -16,12 +35,13 @@ function HomeGuest() {
 					</p>
 				</div>
 				<div className='col-lg-5 pl-lg-5 pb-3 py-lg-5'>
-					<form>
+					<form onSubmit={e => handleSubmit(e)}>
 						<div className='form-group'>
 							<label htmlFor='username-register' className='text-muted mb-1'>
 								<small>Username</small>
 							</label>
 							<input
+								onChange={e => setUsername(e.target.value)}
 								id='username-register'
 								name='username'
 								className='form-control'
@@ -35,6 +55,7 @@ function HomeGuest() {
 								<small>Email</small>
 							</label>
 							<input
+								onChange={e => setEmail(e.target.value)}
 								id='email-register'
 								name='email'
 								className='form-control'
@@ -48,6 +69,7 @@ function HomeGuest() {
 								<small>Password</small>
 							</label>
 							<input
+								onChange={e => setPassword(e.target.value)}
 								id='password-register'
 								name='password'
 								className='form-control'
