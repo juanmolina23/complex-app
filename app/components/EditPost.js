79d628dfbb95b7f8ea50bd66141867a0a73c5551
Juan Molina Jiron <jmolinaj@fiu.edu>
2020-11-13 15:56:28 -0500

    Added client side validation when editing post to prevent user from updating post with empty fields

--- app/components/EditPost.js
+++ app/components/EditPost.js
@@ -37,13 +37,17 @@ function ViewSinglePost() {
 				draft.isFetching = false
 				return
 			case 'titleChange':
+				draft.title.hasErrors = false
 				draft.title.value = action.value
 				return
 			case 'bodyChange':
+				draft.body.hasErrors = false
 				draft.body.value = action.value
 				return
 			case 'submitRequest':
-				draft.sendCount++
+				if (!draft.title.hasErrors && !draft.body.hasErrors) {
+					draft.sendCount++
+				}
 				return
 			case 'saveRequestStarted':
 				draft.isSaving = true
@@ -51,12 +55,30 @@ function ViewSinglePost() {
 			case 'saveRequestFinished':
 				draft.isSaving = false
 				return
+			case 'titleRules':
+				if (!action.value.trim()) {
+					draft.title.hasErrors = true
+					draft.title.message = 'You must provide a title'
+				} else {
+					draft.title.message = ''
+				}
+				return
+			case 'bodyRules':
+				if (!action.value.trim()) {
+					draft.body.hasErrors = true
+					draft.body.message = 'You must provide body content'
+				} else {
+					draft.body.message = ''
+				}
+				return
 		}
 	}
 	const [state, dispatch] = useImmerReducer(ourReducer, originalState)
 
 	function submitHandler(e) {
 		e.preventDefault()
+		dispatch({ type: 'titleRules', value: state.title.value })
+		dispatch({ type: 'bodyRules', value: state.body.value })
 		dispatch({ type: 'submitRequest' })
 	}
 
@@ -129,6 +151,9 @@ function ViewSinglePost() {
 						<small>Title</small>
 					</label>
 					<input
+						onBlur={e =>
+							dispatch({ type: 'titleRules', value: e.target.value })
+						}
 						onChange={e =>
 							dispatch({ type: 'titleChange', value: e.target.value })
 						}
@@ -141,6 +166,11 @@ function ViewSinglePost() {
 						placeholder=''
 						autoComplete='off'
 					/>
+					{state.title.hasErrors && (
+						<div className='alert alert-danger small liveValidateMessage'>
+							{state.title.message}
+						</div>
+					)}
 				</div>
 
 				<div className='form-group'>
@@ -148,6 +178,7 @@ function ViewSinglePost() {
 						<small>Body Content</small>
 					</label>
 					<textarea
+						onBlur={e => dispatch({ type: 'bodyRules', value: e.target.value })}
 						onChange={e =>
 							dispatch({ type: 'bodyChange', value: e.target.value })
 						}
@@ -157,6 +188,11 @@ function ViewSinglePost() {
 						type='text'
 						value={state.body.value}
 					/>
+					{state.body.hasErrors && (
+						<div className='alert alert-danger small liveValidateMessage'>
+							{state.body.message}
+						</div>
+					)}
 				</div>
 
 				<button className='btn btn-primary' disabled={state.isSaving}>
