16963b1d49175fc72e09d889d976102784ca2bea
Juan Molina Jiron <juanmolinajiron@gmail.com>
2021-08-31 11:53:53 -0400

    added new backend url

--- app/Main.js
+++ app/Main.js
@@ -1,12 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './components/App'
-import Axios from 'axios'
+import React from "react"
+import ReactDOM from "react-dom"
+import App from "./components/App"
+import Axios from "axios"
 Axios.defaults.baseURL =
-	process.env.BACKENDURL || 'https://molina-backend-complexapp.herokuapp.com'
+  process.env.BACKENDURL || "https://molina-complexapp-backend.netlify.app"
 
-ReactDOM.render(<App />, document.querySelector('#app'))
+ReactDOM.render(<App />, document.querySelector("#app"))
 
 if (module.hot) {
-	module.hot.accept()
+  module.hot.accept()
 }
